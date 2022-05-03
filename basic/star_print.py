010de1d426f0293c685c6b59d4e1d6384dd42dc2
DOLE1DOLE <nosloth@daum.net>
2022-05-03 11:47:59 +0900

    220503 점심전 커밋이다

--- basic/star_print.py
+++ basic/star_print.py
@@ -1,8 +1,15 @@
 # *찍기
 for x in range(1,6):
-    for y in range(x,6):
+    for y in range(x,5):
         print('*',end='')
     print('')
+
+
+for x in range(1,6):
+    for y in range(x,5):
+        print(' ')
+    for y in range(1,x):
+        print('*', end='')
 
 
 print('Hello',end='')   ##end는 줄 안바꾸고싶을떄. ' ' 공백넣으면 띄어쓰기
